8c1d25efa3c26be23a7a15758e7f8cef845d81b5
Youcef Aouali <you.aouali@gmail.com>
2022-04-22 13:18:55 +0200

    Changes

---
+++ src/review/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.forms import ImageField, RadioSelect
+from . import models
+from django.contrib.auth import get_user_model
+
+
+class NewTicketForm(forms.ModelForm):
+    class Meta:
+        model = models.Ticket
+        fields = ['title', 'description', 'image']
+        widget = {"image": ImageField()}
+
+
+class NewReviewForm(forms.ModelForm):
+    headline = forms.CharField(
+        label="Title",
+        max_length=128,
+        widget=forms.TextInput()
+    )
+    rating = forms.ChoiceField(
+        initial=1,
+        label="Rating",
+        widget=RadioSelect(),
+        choices=((1, "1 étoiles"), (2, "2 étoiles"), (3, "3 étoiles"), (4, "4 étoiles"), (5, '5 étoiles'))
+    )
+    body = forms.CharField(
+        label="Review",
+        max_length=8192,
+        widget=forms.Textarea(),
+        required=False
+    )
+
+    class Meta:
+        model = models.Review
+        fields = ['headline', 'rating', 'body']
+
+
+class FollowUsersForm(forms.ModelForm):
+    User = get_user_model()
+
+    class Meta:
+        model = models.UserFollows
+        fields = ['followed_user']
